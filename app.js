da5a83b6f9492cda07e26095d88a9126cee07e75
Yulin Yu <60793200+yulinyu1120@users.noreply.github.com>
2021-08-28 14:31:33 -0400

    change img src

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dak0crwfy/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
